93356278ccc3612fd9d6f509c4154a705f89554d
Riswan-Nopiyar <btdbattles777@gmail.com>
2022-04-21 12:01:32 +0800

    disposisi

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+
 	/**
 	 * Index Page for this controller.
 	 *
@@ -20,6 +21,15 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+
 		$this->load->view('welcome_message');
 	}
+	public function about() {
+		
+		$this->load->view('about');
+		}
+	public function profile() {
+				
+		$this->load->view('profile');
+		}
 }
